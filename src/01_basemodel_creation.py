6a7dfb4e591312e9386b7ad96e4cebd8ebe47522
premsaisahoo <premsaisahoo@gmail.com>
2022-05-19 16:49:00 +0530

    basemodel done

--- src/01_basemodel_creation.py
+++ src/01_basemodel_creation.py
@@ -6,6 +6,7 @@ import logging
 from src.utils.common import read_yaml, create_directories
 import random
 import tensorflow as tf
+import io
 
 
 STAGE = "creating base model" # <<< change stage name 
@@ -22,14 +23,12 @@ def main(config_path):
     # read config files
     config = read_yaml(config_path)
     # get the data
-    mnist = tf.keras.datasets.mnist
-    (X_train_full, y_train_full), (X_test, y_test) = mnist.load_data()
-    X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+    (X_train_full, y_train_full), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+    X_train_full = X_train_full / 255.0
+    X_test = X_test / 255.0
+    X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
     y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
-
-    # scale the test set as well
-    X_test = X_test / 255.
-    return (X_train,y_train),(X_valid,y_valid),(X_test,y_test)
+    
     seed = 2021
     tf.random.set_seed(seed)
     np.random.seed(seed)
@@ -38,21 +37,49 @@ def main(config_path):
     LAYERS = [
         tf.keras.layers.Flatten(input_shape = [28,28],name = "inputlayer1"),
         tf.keras.layers.Dense(300,name = "hiddenlayer2"),
-        tf.keras.layers.LekyRelu(),
+        tf.keras.layers.LeakyReLU(),
         tf.keras.layers.Dense(100,name = "hiddenlayer3"),
-        tf.keras.layers.LekyRelu(),
+        tf.keras.layers.LeakyReLU(),
         tf.keras.layers.Dense(10,name = "outputlayer")  
         
     ]
 
-  #define the model and compile it
-    LOSS = "categorical_cross_entropy"
+    model = tf.keras.models.Sequential(LAYERS)
+
+
+    LOSS = "sparse_categorical_crossentropy"
     OPTIMIZER = tf.keras.optimizers.SGD(learning_rate=1e-3)
     METRICS = ["accuracy"]
 
-    model = tf.keras.models.Sequential(LAYERS)
-    model.compile(loss = LOSS,optimizer = OPTIMIZER,metrics = METRICS)
-    model.summary()
+    model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS) 
+    ## log our model summary information in logs
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+        # model.summary()
+    logging.info(f"base model summary: \n{_log_model_summary(model)}")
+  
+    ## Train the model
+    history = model.fit(
+        X_train, y_train, 
+        epochs=10, 
+        validation_data=(X_valid, y_valid),
+        verbose=2)
+
+    ## save the base model - 
+    model_dir_path = os.path.join("artifacts","models")
+    create_directories([model_dir_path])
+
+    model_file_path = os.path.join(model_dir_path, "base_model.h5")
+    model.save(model_file_path)
+    logging.info(f"base model is saved at {model_file_path}")
+    logging.info(f"evaluation metrics {model.evaluate(X_test,y_test)}")
+
+
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
